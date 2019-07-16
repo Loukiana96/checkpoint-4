6c867601f7ac14c36ce21f92fef979ec7a867bd5
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-07-16 16:26:05 +0200

    Create UserPage and Gift component.

---
+++ frontend/src/components/UserPage/Gift.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./User.css";
+
+class Gift extends React.Compoment {
+  render() {
+    return <p>Gift</p>;
+  }
+}
+
+export default Gift;
---
+++ frontend/src/components/UserPage/UserPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./User.css";
+import Gift from "./Gift";
+
+class UserPage extends React.Compoment {
+  render() {
+    return <p>UserPage</p>;
+  }
+}
+
+export default UserPage;
